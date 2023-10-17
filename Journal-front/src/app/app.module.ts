410d13509bbc0d11b44d54199a30550b5d4ee93a
Wael Arfaoui <waelarfaoui@ieee.org>
2023-10-17 09:17:09 +0100

    update dashboard component

--- Journal-front/src/app/app.module.ts
+++ Journal-front/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AsideComponent } from './aside/aside.component';
@@ -10,17 +9,12 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { UpdatesComponent } from './updates/updates.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import { AuthorComponent } from './author/author.component';
-import { EditorComponent } from './editor/editor.component';
-import { ReviewerComponent } from './reviewer/reviewer.component';
-import { SubmitManuscriptComponent } from './submit-manuscript/submit-manuscript.component';
-import { AllManuscriptsComponent } from './all-manuscripts/all-manuscripts.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AuthorAsideComponent } from './author/author-aside/author-aside.component';
 import { AuthorDashboardComponent } from './author/author-dashboard/author-dashboard.component';
-import { ManuscriptsComponent } from './author/manuscripts/manuscripts.component';
 import { SettingsComponent } from './settings/settings.component';
 import { AuthorUpdatesComponent } from './author/author-updates/author-updates.component';
+import {AuthorComponent} from "./author/author.component";
 
 @NgModule({
   declarations: [
@@ -28,17 +22,12 @@ import { AuthorUpdatesComponent } from './author/author-updates/author-updates.c
     AsideComponent,
     LoginComponent,
     MainComponent,
+    AuthorComponent,
     SignUpComponent,
     UpdatesComponent,
-    AuthorComponent,
-    EditorComponent,
-    ReviewerComponent,
-    SubmitManuscriptComponent,
-    AllManuscriptsComponent,
     ProfileComponent,
     AuthorAsideComponent,
     AuthorDashboardComponent,
-    ManuscriptsComponent,
     SettingsComponent,
     AuthorUpdatesComponent
   ],
--- Journal-front/src/app/author/author-aside/author-aside.component.ts
+++ Journal-front/src/app/author/author-aside/author-aside.component.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-author-aside',
   templateUrl: './author-aside.component.html',
   styleUrls: ['./author-aside.component.scss']
 })
-export class AuthorAsideComponent {
+export class AuthorAsideComponent implements OnInit{
+
+  constructor(private route:ActivatedRoute,private router:Router) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['dashboard'],{relativeTo:this.route});
+  }
+
   items = [
-    { id: 1,icon:'grid_view' , name: 'Dashbord' },
-    { id: 2,icon:'edit_note' , name: 'Manuscripts' },
-    { id: 3,icon:'add' , name: 'Add Manuscript' },
-    { id: 4,icon:'monitoring' , name: 'Updates' },
-    { id: 5,icon:'chat' , name: 'Profile' },
-    { id: 6,icon:'settings' , name: 'Settings' }
+    { id: 1,icon:'grid_view' , name: 'Dashbord' , url:'dashboard'},
+    { id: 2,icon:'edit_note' , name: 'Manuscripts', url:'manuscripts' },
+    { id: 3,icon:'add' , name: 'Add Manuscript', url:'submit'},
+    { id: 4,icon:'monitoring' , name: 'Updates' , url:'updates'},
+    { id: 5,icon:'chat' , name: 'Profile', url:'profile' },
+    { id: 6,icon:'settings' , name: 'Settings', url:'settings' }
   ];
 
   selectedItemId: number | null = 1;
@@ -20,6 +28,14 @@ export class AuthorAsideComponent {
   selectItem(itemId: number) {
     this.selectedItemId = itemId;
   }
+
+  onPathClick(url:string , itemId:number){
+    this.selectItem(itemId);
+    this.router.navigate([url],{relativeTo:this.route}); }
+
+
+
+
 
 
 }
